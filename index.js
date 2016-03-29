a178fcc05c522d8ab297aed149113e077762f405
Alvin Gao <gao.475@osu.edu>
2016-03-29 17:40:20 -0400

    Debugging CSS

--- index.js
+++ index.js
@@ -24,11 +24,13 @@ server = http.createServer(function (req, res) {
   });
 });
 
+server.use("/css",express.static(__dirname + "/css"));
+
 port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
   this.res.writeHead(200);
-  this.res.end("\<html\>\<head\>\<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"\>\<title\>Banlist Predictions\</title\>\</head\>\<body\>\<p align=\"center\"\>\<h1\>" 
+  this.res.end("\<html\>\<head\>\<link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\"\>\<title\>Banlist Predictions\</title\>\</head\>\<body\>\<p align=\"center\"\>\<h1\>" 
     + bot.banlist() + "\</h1\>\</p\>\</body\>\</html\>");
 }
