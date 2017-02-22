02d99a67cf6c2d227594648206da661a97917c19
Alvin Gao <gaovinal@gmail.com>
2017-02-22 12:07:52 -0500

    tried to fix things

--- bot.js
+++ bot.js
@@ -32,13 +32,11 @@ function respond(request, callback) {
     } else {
       // botResponse = "I'm sorry, I can't do that.";
     }
-    this.res.end();
   } else if(request.text && potOfGreed.test(request.text)) {
     callback("I ACTIVATE POT OF GREED! IT ALLOWS ME TO ADD TWO CARDS FROM MY DECK TO MY HAND.");
   } else {
     console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
+    callback("");
   }
 }
 
@@ -436,43 +434,6 @@ function listContains(list, string) {
   }
 
   return false;
-}
-
-function postMessage(text) {
-  var botResponse, options, body, botReq;
-
-  botResponse = text;
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-  this.res.writeHead(200);
-  this.res.end();
 }
 
 // stackoverflow ftw
